0f495b2d9e80187370c7c8ccff2d8300d3ed66c9
Pavel Maslyaev <pavel-max@rambler.ru>
2016-07-07 17:37:12 +0300

    add files

---
+++ app/src/main/java/com/next/bbauto/Adapters/CarListAdapter.java
@@ -0,0 +1,7 @@
+package com.next.bbauto.Adapters;
+
+/**
+ * Created by maslparu on 07.07.2016.
+ */
+public class CarListAdapter {
+}
---
+++ app/src/main/java/com/next/bbauto/Fragments/CarListFragment.java
@@ -0,0 +1,7 @@
+package com.next.bbauto.Fragments;
+
+/**
+ * Created by maslparu on 07.07.2016.
+ */
+public class CarListFragment {
+}
