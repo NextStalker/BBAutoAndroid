688d5e13cf4d540231ad6a5f4af63c48c7c567fc
Pavel Maslyaev <pavel-max@rambler.ru>
2016-07-07 17:37:36 +0300

    add files

--- app/src/main/java/com/next/bbauto/Adapters/CarListAdapter.java
+++ app/src/main/java/com/next/bbauto/Adapters/CarListAdapter.java
@@ -1,7 +1,33 @@
 package com.next.bbauto.Adapters;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by maslparu on 07.07.2016.
  */
-public class CarListAdapter {
+public class CarListAdapter extends RecyclerView.Adapter<CarListAdapter.ViewHolder> {
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
 }
--- app/src/main/java/com/next/bbauto/Fragments/CarListFragment.java
+++ app/src/main/java/com/next/bbauto/Fragments/CarListFragment.java
@@ -1,7 +1,12 @@
 package com.next.bbauto.Fragments;
 
+import android.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+
 /**
  * Created by maslparu on 07.07.2016.
  */
-public class CarListFragment {
+public class CarListFragment extends Fragment {
+
+    private RecyclerView recyclerView;
 }
